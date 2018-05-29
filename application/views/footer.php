bc595ce9c1412ecdac67e040ec6ca62a4b6a0ba1
Unknown <mauryecc@gmail.com>
2018-05-28 23:38:54 -0400

    Login
    Aun esta incompleto, la estructura con boostrap esta listo

---
+++ application/views/footer.php
@@ -0,0 +1,26 @@
+</div>
+</div>
+ <div id="footer">
+      <div class="container" style="text-align:center; font-size:12px;">
+        <p>Bienvenido <font style="color:red"><?=$nombre?></font><br><a href="<?=base_url()?>Principal/logout">Cerrar Sesión</a></p>
+        <p class="muted credit"><br>Desarrollado por <a href="#">www.morelike.cl</a></p>
+      </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+    <!--script src="<?=base_url()?>bootstrap/assets/js/bootstrap-transition.js"></script>
+    <script src="<?=base_url()?>bootstrap/assets/js/bootstrap-alert.js"></script>
+    <script src="<?=base_url()?>bootstrap/assets/js/bootstrap-modal.js"></script>
+    <script src="<?=base_url()?>bootstrap/assets/js/bootstrap-dropdown.js"></script>
+    <script src="<?=base_url()?>bootstrap/assets/js/bootstrap-scrollspy.js"></script>
+    <script src="<?=base_url()?>bootstrap/assets/js/bootstrap-tab.js"></script>
+    <script src="<?=base_url()?>bootstrap/assets/js/bootstrap-tooltip.js"></script>
+    <script src="<?=base_url()?>bootstrap/assets/js/bootstrap-popover.js"></script>
+    <script src="<?=base_url()?>bootstrap/assets/js/bootstrap-button.js"></script>
+    <script src="<?=base_url()?>bootstrap/assets/js/bootstrap-collapse.js"></script>
+    <script src="<?=base_url()?>bootstrap/assets/js/bootstrap-carousel.js"></script>
+    <script src="<?=base_url()?>bootstrap/assets/js/bootstrap-typeahead.js"></script-->
+
+  </body>
+</html>
