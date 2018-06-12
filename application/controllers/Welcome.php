41a5ad095be88ed5bbaab6a3cb9dfce42227b05c
Carlosccccs <carloc_c2@hotmail.com>
2018-06-12 19:45:01 -0400

    # Comentarios al codigo del login
    * Se agregaron comentarios al metodo del login para ayudar a su entendimiento y cumplir con las normas de desarrollo de este proyecto

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,29 +18,41 @@ class Welcome extends CI_Controller {
 		$this->load->view('VistaLogin');
 	}
 
+    /*
+    Metodo que busca a el usuario en la base de datos, si existe muestra la vista docente, en caso contrario,
+    Manda un mensaje de error
+    */
 	function validaUsuario(){
+        //Recibe los datos del formulario
         $usuario = $this->input->post("rut");
         $clave =  $this->input->post('clave');
+        //Busca el usuario
         $valor = $this->GestionModel->ConsultaDocente($usuario,$clave)->num_rows();
+        //Si el la consulta devuelve mas de un resultado = TRUE
         if($valor == 1){
-            //Buscar nombre del usuario que se ha conectado....
+            //Extraer los datos de la consulta a un arreglo
             $res = $this->GestionModel->ConsultaDocente($usuario,$clave)->result();
+            //Se extraen los datos del arreglo y se asignan a variables
             foreach ($res as $row) {
                 $Rut = $row->Rut;
 				$Nombre = $row->Nombre;
 				$Descripcion = $row->Descripcion;
             }
+            //Se preparan los datos para guardarlos en la session (Pendiente de revision en logged_in)
             $data   =   array(
                 'Rut'  => $Rut,
                 'nombre'    => $Nombre,
                 'logged_in' => TRUE,
                 'Descripcion' => $Descripcion
             );
+            //Lineas pendientes de revision
         $this->session->set_userdata($data);
             $data['Nombre'] = $this->session->userdata('nombre');
             $data['Descripcion'] = $this->session->userdata("Descripcion");
+            //Carga la vista del docente
             $this->load->view("VistaDocente");
         }else{
+            //Manda un mensaje y vuelve a cargar la vista de login
             $data['error'] = "Datos incorrectos o no existen";
             $this->load->view('VistaLogin',$data);
 		}
