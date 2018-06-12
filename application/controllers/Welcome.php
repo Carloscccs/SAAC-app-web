f535bc6d3758eeffb14a41b74ea925b01e667fcc
Carlosccccs <carloc_c2@hotmail.com>
2018-06-11 21:15:21 -0400

    # Login funcionando parcialmente
    * Se le dio funcionalidad al login, pero falta definir los datos enviar a la session
    * Redirecciona al modulo docente
    * Muestra un mensaje de error si no encuentra el usuario

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,6 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('GestionModel');
+	}
 	
 	public function index()
 	{
@@ -12,5 +16,33 @@ class Welcome extends CI_Controller {
 	public function CargarVistaLogin()
 	{
 		$this->load->view('VistaLogin');
+	}
+
+	function validaUsuario(){
+        $usuario = $this->input->post("rut");
+        $clave =  $this->input->post('clave');
+        $valor = $this->GestionModel->ConsultaDocente($usuario,$clave)->num_rows();
+        if($valor == 1){
+            //Buscar nombre del usuario que se ha conectado....
+            $res = $this->GestionModel->ConsultaDocente($usuario,$clave)->result();
+            foreach ($res as $row) {
+                $Rut = $row->Rut;
+				$Nombre = $row->Nombre;
+				$Descripcion = $row->Descripcion;
+            }
+            $data   =   array(
+                'Rut'  => $Rut,
+                'nombre'    => $Nombre,
+                'logged_in' => TRUE,
+                'Descripcion' => $Descripcion
+            );
+        $this->session->set_userdata($data);
+            $data['Nombre'] = $this->session->userdata('nombre');
+            $data['Descripcion'] = $this->session->userdata("Descripcion");
+            $this->load->view("VistaDocente");
+        }else{
+            $data['error'] = "Datos incorrectos o no existen";
+            $this->load->view('VistaLogin',$data);
+		}
 	}
 }
--- application/views/VistaLogin.php
+++ application/views/VistaLogin.php
@@ -19,16 +19,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
   </head>
   <body id="bodylogin" class="text-center">
-	<form class="form-signin bg-light rounded">
+	<form class="form-signin bg-light rounded" method="post" action="<?php base_url();?>login">
 		<img class="mb-4" src="<?php echo base_url(); ?>img/logo-temporal.png" alt="" width="112" height="72">
 		<h1 class="h3 mb-3 font-weight-normal">Bienvenido</h1>
 		<label for="inputRut" class="sr-only">Rut</label>
-		<input type="text" id="inputRut" class="form-control" placeholder="Rut" required autofocus>
+		<input type="text" name="rut" id="inputRut" class="form-control" placeholder="Rut" required autofocus>
 		<label for="inputPassword" class="sr-only">Contraseña</label>
-		<input type="password" id="inputPassword" class="form-control" placeholder="Constraseña" required>
+		<input type="password" name="clave" id="inputPassword" class="form-control" placeholder="Constraseña" required>
 		<button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
 		<p class="mt-5 mb-3 text-muted">&copy; SAAC app 2018</p>
+    <label class="error" style="color:#f00; font-size:15px; text-align: center; font-weight: bold; margin-top: 20px;"><?php if (isset($error)) {echo $error;}?></label>
 		</form>
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
